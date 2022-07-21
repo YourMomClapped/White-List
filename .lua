6ca41c03df950453e3765d9ada575ef10a58ca4f
YourMomClapped <91243291+YourMomClapped@users.noreply.github.com>
2022-07-21 16:49:12 +0930

    Update .lua

--- .lua
+++ .lua
@@ -1,7 +1,7 @@
 local Player = game.Players.LocalPlayer
 
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
-  local Window = OrionLib:MakeWindow({Name = "Annon Hub whitelist", HidePremium = false, IntroText = "Annon Hub", SaveConfig = true, ConfigFolder = "OrionTest"})
+  local Window = OrionLib:MakeWindow({Name = "Annon Hub whitelist", HidePremium = false, IntroText = "Annon Hub", SaveConfig = false, ConfigFolder = "OrionTest"})
   
   -- Notifications
   OrionLib:MakeNotification({
